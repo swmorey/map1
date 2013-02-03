17f12fe404efd60d5ed224768188019b2fd47c6e
Scott W. Morey <swmorey@gmail.com>
2013-02-03 01:54:31 -0500

    helloworld

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Map::Application.config.secret_token = '752d1feecb61a8132354e902ba0ec7ae21aa064e60967a8ceb3bf7551bb8c444745bcaa971dbef4d1ca430d067521266129d1fd93bf53cb1ad35dfa4d52d9609'
